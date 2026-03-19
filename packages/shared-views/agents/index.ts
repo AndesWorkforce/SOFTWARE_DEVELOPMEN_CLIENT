0e557c4e08f5871898fa80c79a445a802b46c13c
David Morcillo <aljodavid100@gmail.com>
2026-03-19 02:32:07 -0500

    fix: url domain

--- packages/shared-views/agents/index.ts
+++ packages/shared-views/agents/index.ts
@@ -1,3 +1,4 @@
 export { AgentsManagementView, type AgentsManagementViewProps } from "./AgentsManagementView";
 export { AppAssignmentView, type AppAssignmentViewProps } from "./AppAssignmentView";
 export { DomainAssignmentView, type DomainAssignmentViewProps } from "./DomainAssignmentView";
+export { UrlManagementView, type UrlManagementViewProps } from "./UrlManagementView";
